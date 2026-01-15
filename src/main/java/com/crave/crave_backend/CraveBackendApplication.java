d668c219c7a045c69fb860333bca1b2ab0efd70e
Krishna <krishnagupta2322@gmail.com>
2026-01-15 15:46:03 +0530

    Spring Initializer Folder Structure

---
+++ src/main/java/com/crave/crave_backend/CraveBackendApplication.java
@@ -0,0 +1,12 @@
+package com.crave.crave_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraveBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/crave/crave_backend/CraveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crave.crave_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
